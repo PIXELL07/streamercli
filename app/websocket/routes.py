4346c835067bf5a3550abc85935e71d6d24e5194
PIXELL07 <arunksn007@gmail.com>
2026-03-30 20:26:38 +0530

    feat: updated token & error handling logic

--- app/websocket/routes.py
+++ app/websocket/routes.py
@@ -2,29 +2,33 @@ from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 import json
 import asyncio
 
+
 from app.websocket.manager import ConnectionManager
 from app.websocket.handlers import handle_message
 from app.redis.pubsub import redis_listener
+from fastapi import WebSocket
+from app.auth.jwt_handler import verify_token
 
 router = APIRouter()
 manager = ConnectionManager()
 
 
-@router.websocket("/ws/{user}")
-async def websocket_endpoint(websocket: WebSocket, user: str):
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+
+    token = websocket.query_params.get("token")
+
+    data = verify_token(token)
+    user = data["user"]
 
     await manager.connect(user, websocket)
-
     try:
         while True:
             data = await websocket.receive_text()
-            data = json.loads(data)
-
-            await handle_message(manager, user, data)
-
+            message = json.loads(data)
+            await handle_message(user, message, manager)
     except WebSocketDisconnect:
         manager.disconnect(user)
-
 
 @router.on_event("startup")
 async def start_listener():
